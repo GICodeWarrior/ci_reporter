fed2f8e02320ae213b4be6cc088f528e25f4fc2e
Rusty Burchfield <GICodeWarrior@gmail.com>
2009-09-03 10:22:39 -0700

    Remove spliced in progress format.  If we want progress we will pass it as another format option to rspec.

--- lib/ci/reporter/rspec.rb
+++ lib/ci/reporter/rspec.rb
@@ -6,7 +6,6 @@ require 'ci/reporter/core'
 tried_gem = false
 begin
   require 'spec'
-  require 'spec/runner/formatter/progress_bar_formatter'
   require 'spec/runner/formatter/specdoc_formatter'
 rescue LoadError
   unless tried_gem
@@ -41,38 +40,31 @@ module CI
     # Custom +RSpec+ formatter used to hook into the spec runs and capture results.
     class RSpec < Spec::Runner::Formatter::BaseFormatter
       attr_accessor :report_manager
-      attr_accessor :formatter
       def initialize(*args)
         super
-        @formatter ||= Spec::Runner::Formatter::ProgressBarFormatter.new(*args)
         @report_manager = ReportManager.new("spec")
         @suite = nil
       end
 
       def start(spec_count)
-        @formatter.start(spec_count)
       end
 
       # rspec 0.9
       def add_behaviour(name)
-        @formatter.add_behaviour(name)
         new_suite(name)
       end
 
       # Compatibility with rspec < 1.2.4
       def add_example_group(example_group)
-        @formatter.add_example_group(example_group)
         new_suite(example_group.description)
       end
 
       # rspec >= 1.2.4
       def example_group_started(example_group)
-        @formatter.example_group_started(example_group)
         new_suite(example_group.description)
       end
 
       def example_started(name)
-        @formatter.example_started(name)
         name = name.description if name.respond_to?(:description)
         spec = TestCase.new name
         @suite.testcases << spec
@@ -80,44 +72,36 @@ module CI
       end
 
       def example_failed(name, counter, failure)
-        @formatter.example_failed(name, counter, failure)
         spec = @suite.testcases.last
         spec.finish
         spec.failures << RSpecFailure.new(failure)
       end
 
       def example_passed(name)
-        @formatter.example_passed(name)
         spec = @suite.testcases.last
         spec.finish
       end
 
       def example_pending(*args)
-        @formatter.example_pending(*args)
         spec = @suite.testcases.last
         spec.finish
         spec.name = "#{spec.name} (PENDING)"
       end
 
       def start_dump
-        @formatter.start_dump
       end
 
       def dump_failure(*args)
-        @formatter.dump_failure(*args)
       end
 
       def dump_summary(*args)
-        @formatter.dump_summary(*args)
         write_report
       end
 
       def dump_pending
-        @formatter.dump_pending
       end
 
       def close
-        @formatter.close
       end
 
       private
